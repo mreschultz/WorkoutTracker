1f21dc29ea0c4f5483bcf48101777a60234c2759
Eric Schultz <mreschultz@me.com>
2020-11-18 15:57:03 -0500

    make it better

--- Develop/server.js
+++ Develop/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-     process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+     process.env.MONGODB_URI || 'mongodb://localhost/27017',
      {
        useNewUrlParser: true,
        useUnifiedTopology: true,
