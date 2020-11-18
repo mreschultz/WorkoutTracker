e5255d80f897161769223f00ab5af0e89aa6be5b
Eric Schultz <mreschultz@me.com>
2020-11-18 16:20:28 -0500

    make it better

--- Develop/server.js
+++ Develop/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-     process.env.MONGODB_URI || 'mongodb://localhost/27017',
+     process.env.MONGODB_URI || 'mongodb://localhost/Workout',
      {
        useNewUrlParser: true,
        useUnifiedTopology: true,
